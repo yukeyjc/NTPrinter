85fb1618f6782232cccee0b13c6ea0c5da6d253f
Ayukey <295605703@qq.com>
2022-11-03 10:19:50 +0800

    小票模板更新

--- Demo/Demo/ViewController.m
+++ Demo/Demo/ViewController.m
@@ -89,4 +89,48 @@
     }];
 }
 
+- (IBAction)printTicket:(id)sender {
+    NSDictionary *data = @{
+        @"SName":@"西湖小店",
+        @"SAddress":@"中国浙江省杭州市余杭区西子湖街道 西湖路3999号",
+        @"SPhone":@"1380013800",
+        @"Code":@"asdasdasdasdasdasdasd",
+        @"STime":@"2022-10-21",
+        @"PTime":@"2022-10-25",
+        @"RePrint":@YES,
+        @"DType":@"正常/退货./换货",
+        @"SNumber":@"S123123123",
+        @"DNumber":@"P123123123",
+        @"PName":@"张三",
+        @"PCard":@"AE_71827381923_KJ",
+        @"PPoint":@"2590",
+        @"PayList":@[
+            @[@"L1212-98 001-4", @"1190.00", @"100", @"1", @"1190.00"],
+            @[@"L1212-98 001-5", @"1190.00", @"100", @"1", @"1190.00"],
+            @[@"L1212-98 001-6", @"1190.00", @"100", @"1", @"1190.00"],
+            @[@"L1212-98 001-7", @"1190.00", @"100", @"1", @"1190.00"],
+            @[@"L1212-98 001-8", @"1190.00", @"100", @"1", @"1190.00"],
+        ],
+        @"TotalCount":@"4",
+        @"WXPay":@"2000.00",
+        @"TotalMoney":@"4760.00",
+        @"CardPay":@"2000.00",
+        @"TotalPay":@"4760.00",
+        @"CashPay":@"760.00",
+        @"Change":@"0.00",
+        @"Writing":@"阿贾克斯队将阿克琉斯经典款辣椒",
+        @"WCode":@"https://nsev.inecm.cn/lacoste/print-template/logo.jpg",
+        @"PWriting":@"佳卡拉斯京的考拉就是快乐的",
+        @"PCode":@"https://nsev.inecm.cn/lacoste/print-template/logo.jpg",
+        @"MWriting":@"阿就是看鹿鼎记阿克琉斯几点开链接",
+        @"MCode":@"https://nsev.inecm.cn/lacoste/print-template/logo.jpg",
+    };
+    
+    [[NTPrinter shared] printTemplateWithName:@"GPC08180_T2"  templateData:data completion:^(NTError * _Nullable  error) {
+        if (error == nil) {
+            NSLog(@"发送打印指令成功");
+        }
+    }];
+}
+
 @end
